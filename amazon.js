0f0dc2d9b5a4e37283e6a4cf58d23707a678e087
Sai Siva <nandurisaisiva8@.com>
2024-10-06 13:16:19 +0530

    Move add to cart.js

--- amazon.js
+++ amazon.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart,addToCart} from '../data/cart.js';
 import {products} from '../data/products.js'
 let productsHTML='';
 products.forEach((product) =>{
@@ -65,47 +65,34 @@ document.querySelector('.js-product-grid').innerHTML=productsHTML;
 
 const addedMessageTimeouts = {};
 
+
+
+
+
+
+
+function updateCartQuantity(){
+  let cartQuantity = 0; 
+   
+   
+  cart.forEach((cartItem) => {
+     cartQuantity += cartItem.quantity;
+
+     document.querySelector('.js-cart-quantity')
+    .innerHTML =cartQuantity;
+
+  });
+}
+
+
+
+
 document.querySelectorAll('.js-add-to-cart')
 .forEach((button) =>{
   button.addEventListener('click',() =>{
    const {productId}= button.dataset;
-
-   let matchingItem;
-
-    cart.forEach((item)=> {
-      if (productId===item.productId){
-          matchingItem=item;
-      }
-
-    });
-
-
-    const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
-
-
-    const quantity = Number(quantitySelector.value);
-
-    if  (matchingItem){
-      matchingItem.quantity  += quantity;
-    }else{
-      cart.push({
-        // productId:productId,
-        // quantity:quantity
-        productId,
-        quantity
-       });
-    }
-
-   let cartQuantity = 0; 
-   
-   
-   cart.forEach((item) => {
-      cartQuantity += item.quantity;
-
-   });
-
-    document.querySelector('.js-cart-quantity')
-    .innerHTML =cartQuantity;
+   addToCart(productId);
+   updateCartQuantity();
 
    const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
 
