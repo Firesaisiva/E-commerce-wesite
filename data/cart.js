b57335f4103dfcc13edeb65d49b16f5df51dbb67
Sai Siva <nandurisaisiva8@.com>
2024-10-06 16:58:33 +0530

    Save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,16 +1,24 @@
-export let cart=[{
+export let cart= JSON.parse(localStorage.getItem('cart'));
 
 
-  productId:
-  'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+if (!cart){
 
-  quantity:2,
+  cart = [{
+    productId:
+    'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity:2,
+  
+  },{
+    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  
+    quantity:1
+  }];
+}
 
-},{
-  productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
 
-  quantity:1
-}];
+}
 export function addToCart(productId){
 
 
@@ -41,6 +49,9 @@ export function addToCart(productId){
 
     } 
 
+
+    saveToStorage();
+
 }
 
 export function removeFromCart(productId){
@@ -53,4 +64,5 @@ export function removeFromCart(productId){
   });
 
   cart = newCart;
+  saveToStorage();
 };
