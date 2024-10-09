28206b7c6b68f68ee1740b81cfdf10c488ed4057
Sai Siva <nandurisaisiva8@.com>
2024-10-09 18:33:09 +0530

    Checkout cart quantity working

--- checkout.js
+++ checkout.js
@@ -119,7 +119,19 @@ document.querySelectorAll('.js-delete-link')
 
 
      container.remove();
-
+      updateCartQuantity();
 
     });
   });
+
+  export function updateCartQuantity() {
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+  
+  document.querySelector('.js-return-to-home-link')
+    .innerHTML = `${cartQuantity} items`;
+}
+ updateCartQuantity(); 
